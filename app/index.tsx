c34fb7d6c2a8369492a13dc80dba76e6c9bd19f0
Abhishek Kapoor <kapoorkk1980@gmail.com>
2025-03-04 17:57:30 +0530

    Removed Boilerplate code and added NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 h-full items-center justify-center bg-gray-100"
+    >
+      
+    </View>
+  );
+}
